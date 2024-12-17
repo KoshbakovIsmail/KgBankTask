6802ae1542c06d49173509b597081a8bb046517e
KosbakovIsmail <kosbakovismail@gmail.com>
2024-12-17 23:55:21 +0300

    initial commit

---
+++ src/main/java/com/Task/demoKgBank/DemoKgBankApplication.java
@@ -0,0 +1,13 @@
+package com.Task.demoKgBank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoKgBankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoKgBankApplication.class, args);
+	}
+
+}
